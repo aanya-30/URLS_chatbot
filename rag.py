75fa02aff3c3de1a804eab70ef3efc65dc4a2ef8
aanya.30 <aanyag94@gmail.com>
2025-08-24 17:36:06 +0530

    My second commit

--- rag.py
+++ rag.py
@@ -1,6 +1,5 @@
-#IMPORTING LIBRARIES
-
 from uuid import uuid4
+
 from dotenv import load_dotenv
 from pathlib import Path
 from langchain.chains import RetrievalQAWithSourcesChain
@@ -14,87 +13,102 @@ import os
 
 load_dotenv()
 
-CHUNK_SIZE = 1000
+CHUNK_SIZE=1000
 EMBEDDING_MODEL="Alibaba-NLP/gte-base-en-v1.5"
-VECTOR_STORE_DIR = Path("__file__"),"parent/resources/vector_store"
-COLLECTION_NAME = "real_estate"
+VECTOR_STORE_DIR=Path(__file__).parent/"resources/vector_store"
+COLLECTION_NAME="real_estate"
 hf_token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
-#ENSURE TOKEN IS SET
+# Ensure token is set
 if not hf_token:
-    raise ValueError("Hugging Face API token not found. Please set the HUGGINGFACEHUB_API_TOKEN in .env or a .")
+    raise ValueError("Hugging Face API token not found. Set HUGGINGFACEHUB_API_TOKEN in .env or system environment.")
 
-#AUTHENTICATE BEFORE USING THE MODEL
+# Authenticate before using the model
+login(token=hf_token)
 
-login(hf_token)
 llm=None
 vector_store=None
 
 def initialize_components():
-    global llm, vector_store
-   
+    global llm,vector_store
+
     if llm is None:
-        llm = ChatGroq(model="llama-3.3-70b-versatile",temperature=0.9, max_tokens=500)
+        llm=ChatGroq(model="llama-3.3-70b-versatile",temperature=0.9, max_tokens=500)
 
     if vector_store is None:
-        ef=HuggingFaceEmbeddings(
+        ef= HuggingFaceEmbeddings(
             model_name=EMBEDDING_MODEL,
-            model_kwargs={"trust_remote_code": True},
+            model_kwargs={"trust_remote_code":True}
         )
 
-        vector_store = Chroma(
+        vector_store=Chroma(
             collection_name=COLLECTION_NAME,
             embedding_function=ef,
-            persist_directory=str(VECTOR_STORE_DIR) 
-       )
+            persist_directory=str(VECTOR_STORE_DIR)
+
+        )
+
 
 def process_urls(urls):
     """
-
-    This function scrapes data from the urls and stores it in a vectpr db
+    This function scraps data from the urls and stores it in a vector db
     :param urls: input urls
     :return
     """
 
-    #PRINT ("initialize components")
+    # print("Initialize components")
     yield "Initializing components...✅"
     initialize_components()
     vector_store.reset_collection()
 
-    #PRINT ("load data from urls")
+    # print("load data")
     yield "Resetting vector store...✅"
     loader=UnstructuredURLLoader(urls=urls)
     data=loader.load()
 
-    #PRINT ("split text")
+    # print("Split Text")
     yield "Splitting text into chunks...✅"
     text_splitter=RecursiveCharacterTextSplitter(
-        separators=["\n\n", "\n", ".", ""],
-        chunk_size=CHUNK_SIZE,
+        separators=["\n\n","\n","."," "],
+        chunk_size=CHUNK_SIZE
     )
 
 
     docs=text_splitter.split_documents(data)
-    
-    #PRINT ("Add docs to vector db")
-    yield "Adding chunks to vector store...✅"
+
+    # print("Add docs to vector db")
+    yield "Adding chunks to vector database...✅"
     uuids=[str(uuid4()) for _ in range(len(docs))]
     vector_store.add_documents(docs,ids=uuids)
-
+   
     yield "Done adding docs to vector database...✅"
 
 def generate_answer(query):
     if not vector_store:
-        raise ValueError("Vector database is not initialized")
-
+        raise RuntimeError("Vector Database is not initialized")
+   
     chain=RetrievalQAWithSourcesChain.from_llm(llm=llm,retriever=vector_store.as_retriever())
     result=chain.invoke({"question":query}, return_only_outputs=True)
     sources=result.get("sources","")
 
-    return result["answer"], sources
-if __name__ == "__main__":
+    return result['answer'],sources
+
+if __name__=="__main__":
+    urls=[
+        # "https://www.cnbc.com/2024/12/21/how-the-federal-reserves-rate-policy-affects-mortgages.html&quot;,
+        # "https://www.cnbc.com/2024/12/20/why-mortgage-rates-jumped-despite-fed-interest-rate-cut.html&quot;
+        #"https://www.foxbusiness.com/personal-finance/todays-mortgage-rates-august-14-2024&quot;,
+        #"https://www.foxbusiness.com/personal-finance/todays-mortgage-rates-august-13-2024&quot;
+    ]  #This will process the urls
 
     process_urls(urls)
-    answer, sources = generate_answer("Tell me what was the 30 year fixed mortage rate along with the date?")
+
+    # results=vector_store.similarity_search(
+    #     "30 year mortgage rate",
+    #     k=2
+    # )
+    # print(results)
+
+    answer,sources=generate_answer("Tell me what was the 30 year fixed mortgate rate along with the date?")
     print(f"Answer:{answer}")
-    print(f"Sources:{sources}")
+    print(f"sources:{sources}")
